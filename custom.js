1733921c269f85735b8651d11aa6d403c95e8b0e
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-17 13:19:54 +0530

    sir L-41 assignment done

---
+++ custom.js
@@ -0,0 +1,4 @@
+var name1 = "Lucky"; //declaration and initialization
+console.log(name1);
+var u = 3;
+console.log(u);
---
+++ custom.ts
@@ -0,0 +1,8 @@
+
+var name1:string="Lucky"; //declaration and initialization
+
+console.log(name1);
+
+let u:number = 3;
+
+console.log(u);
